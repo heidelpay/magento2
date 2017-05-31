c28f3ddad3dd1db89de383f76d28a611804fc6d0
Stephano Vogel <stephano.vogel@outlook.com>
2017-05-31 15:16:47 +0200

    Travis-Scripts: Add memory-limit -1 line back

--- dev/travis/before_install.sh
+++ dev/travis/before_install.sh
@@ -6,4 +6,5 @@ trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit
 # disable xdebug and adjust memory limit
 echo '==> Disabling xdebug, adjusting memory limit to -1.'
 phpenv config-rm xdebug.ini
+echo 'memory_limit = -1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 phpenv rehash;
