2a89adc5495a71f5f361520a3fa9ccc3a1a1087d
Simon Gabriel <simon_gabriel@gmx.de>
2018-03-20 17:57:20 +0100

    [refactor] (MAGE2-21) Refactor BasketApiTest to assert that the values sent to the basket api correspond to the values sent to the payment api.

--- Test/Integration/BasketApiTest.php
+++ Test/Integration/BasketApiTest.php
@@ -11,26 +11,27 @@
  */
 namespace Heidelpay\Gateway\Test\Integration;
 
-use Heidelpay\Gateway\Gateway\Config\HgwInvoiceSecuredPaymentConfig;
-use Heidelpay\Gateway\Gateway\Config\HgwMainConfig;
-use Heidelpay\Gateway\PaymentMethods\HeidelpayInvoiceSecuredPaymentMethod;
-use Magento\Framework\App\Config\ScopeConfigInterface;
+use Heidelpay\Gateway\Helper\BasketHelper;
+use Heidelpay\Gateway\Helper\Payment;
+use Heidelpay\PhpBasketApi\Object\Basket;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Model\Product\Type;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Model\Product;
+use Magento\Checkout\Model\Cart;
+use Magento\Customer\Api\Data\CustomerInterfaceFactory;
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Model\Session;
+use Magento\Quote\Model\Quote\Address;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\TestCase\AbstractController;
-use TddWizard\Fixtures\Catalog\ProductBuilder;
-use TddWizard\Fixtures\Catalog\ProductFixture;
-use TddWizard\Fixtures\Catalog\ProductFixtureRollback;
-use TddWizard\Fixtures\Checkout\CartBuilder;
-use TddWizard\Fixtures\Customer\CustomerBuilder;
-use TddWizard\Fixtures\Customer\CustomerFixture;
-use TddWizard\Fixtures\Customer\CustomerFixtureRollback;
 
 class BasketApiTest extends AbstractController
 {
     const NUMBER_OF_PRODUCTS = 5;
 
     /**
-     * @var CustomerFixture $customerFixture
+     * @var Customer $customerFixture
      */
     private $customerFixture;
 
@@ -39,108 +40,134 @@ class BasketApiTest extends AbstractController
      */
     private $productFixtures = [];
 
-    /** @var \Magento\TestFramework\ObjectManager $objectManager*/
-    private $objectManager;
-
     /**
      * {@inheritDoc}
      */
     public function setUp()
     {
-        $this->objectManager = Bootstrap::getObjectManager();
-
-        $this->customerFixture = new CustomerFixture(
-            CustomerBuilder::aCustomer()->build()
-        );
-
-        for ($idx = 0; $idx < self::NUMBER_OF_PRODUCTS; $idx++) {
-            $price = mt_rand(1, 30000) / 100;
-            echo "\nPreis:" . $price;
-            $this->productFixtures[] = new ProductFixture(
-                ProductBuilder::aSimpleProduct()
-                    ->withPrice($price)
-                    ->build()
-            );
-        }
+        $this->_objectManager = Bootstrap::getObjectManager();
+        $this->generateCustomerFixture();
+        $this->generateProductFixtures(self::NUMBER_OF_PRODUCTS);
     }
 
     /**
-     * {@inheritDoc}
-     */
-    public function tearDown()
-    {
-        CustomerFixtureRollback::create()->execute($this->customerFixture);
-
-        foreach ($this->productFixtures as $productFixture) {
-            ProductFixtureRollback::create()->execute($productFixture);
-        }
-    }
-
-    /**
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     * @magentoConfigFixture  default  currency/options/allow EUR
+     * @magentoConfigFixture  default  currency/options/base EUR
+     * @magentoConfigFixture  default  currency/options/default EUR
+     *
      * @test
+     * @throws \Heidelpay\PhpBasketApi\Exception\InvalidBasketitemPositionException
      */
     public function verifySomething()
     {
-        $mainConfigMock = $this->mockMainConfig();
+        /** @var Session $magentoCustomerSession */
+        $magentoCustomerSession = $this->getObject(Session::class);
+        $loggedIn = $magentoCustomerSession->loginById($this->customerFixture->getId());
 
-        $configMock = $this->getMock(
-            HgwInvoiceSecuredPaymentConfig::class,
-            ['getChannel'],
-            [],
-            '',
-            false
-        );
-        $configMock->method('getChannel')->willReturn('31HA07BC81856CAD6D8E05CDDE7E2AC8');
+        $this->assertTrue($loggedIn, 'Could not log in');
 
-        // override config preference to inject custom config parameters
-        $this->objectManager->addSharedInstance(
-            $configMock,
-            'HgwInvoiceSecuredConfig' // wenn das nicht geht, dann die MockConfig::class hier verwenden
-        );
-
-        $this->customerFixture->login();
-
-        $cart = CartBuilder::forCurrentSession();
+        /** @var Cart $cart */
+        $cart = $this->createObject(Cart::class);
         foreach ($this->productFixtures as $productFixture) {
-            $cart = $cart->withSimpleProduct($productFixture->getSku(), mt_rand(1, 3));
+            $cart = $cart->addProduct($productFixture, ['qty' => mt_rand(1, 3)]);
         }
-        $cart = $cart->build();
+        $cart = $cart->save();
         $quote = $cart->getQuote();
 
-        /** @var HeidelpayInvoiceSecuredPaymentMethod $method */
-        $method = $this->objectManager->create(
-            HeidelpayInvoiceSecuredPaymentMethod::class,
-            ['config' => $configMock, 'mainConfig' => $mainConfigMock]
+        /** @var BasketHelper $basketHelper */
+        $basketHelper = $this->getObject(BasketHelper::class);
+
+        /** @var Payment $paymentHelper */
+        $paymentHelper = $this->getObject(Payment::class);
+
+        /** @var Basket $basket */
+        $basket = $basketHelper->convertQuoteToBasket($quote)->getBasket();
+
+        $this->assertEquals(
+            (int)bcmul($paymentHelper->format($quote->getGrandTotal()), 100),
+            $basket->getAmountTotalNet() + $basket->getAmountTotalVat()
         );
-
-        /** @var \Heidelpay\PhpPaymentApi\Response $response */
-        $response = $method->getHeidelpayUrl($quote);
-
-
-
-        echo "\nResponse: " . $response->toJson();
     }
 
-    private function mockMainConfig()
+    private function generateCustomerFixture()
     {
-        $scopeConfig = $this->objectManager->get(ScopeConfigInterface::class);
+        /** @var CustomerFactory $customerFactory */
+        $customerFactory = $this->getObject('\Magento\Customer\Model\CustomerFactory');
 
-        $configMock = $this->getMock(
-            HgwMainConfig::class,
-            ['getSecuritySender', 'getUserPasswd', 'getUserLogin', 'isSandboxModeActive'],
-            ['scopeConfig' => $scopeConfig]
-        );
-        $configMock->method('getUserLogin')->willReturn('31ha07bc8142c5a171744e5aef11ffd3');
-        $configMock->method('getUserPasswd')->willReturn('93167DE7');
-        $configMock->method('getSecuritySender')->willReturn('31HA07BC8142C5A171745D00AD63D182');
-        $configMock->method('isSandboxModeActive')->willReturn(true);
+        /** @var Customer $customer */
+        $customer = $customerFactory->create();
+        $customer
+            ->setEmail('l.h@mail.com')
+            ->setFirstname('Linda')
+            ->setLastname('Heideich')
+            ->setPassword('123456789');
+        $customer->save();
 
-        // override mainconfig preference to inject custom config parameters
-        $this->objectManager->addSharedInstance(
-            $configMock,
-            HgwMainConfig::class // wenn das nicht geht, dann die MockConfig::class hier verwenden
-        );
+        $addressFactory = $this->getObject('\Magento\Customer\Model\AddressFactory');
 
-        return $configMock;
+        /** @var Address $address */
+        $address = $addressFactory->create();
+        $address
+            ->setCustomerId($customer->getId())
+            ->setFirstname('Linda')
+            ->setLastname('Heideich')
+            ->setCountryId('DE')
+            ->setPostcode('69115')
+            ->setCity('Heidelberg')
+            ->setTelephone(1234567890)
+            ->setFax(123456789)
+            ->setStreet('Vangerowstr. 18')
+            ->setIsDefaultBilling('1')
+            ->setIsDefaultShipping('1')
+            ->setSaveInAddressBook('1');
+        $address->save();
+
+        $this->customerFixture = $customer;
+    }
+
+    private function generateProductFixtures($number)
+    {
+        // create product fixtures
+        for ($idx = 1; $idx <= $number; $idx++) {
+            $price = mt_rand(1, 30000) / 100;
+            echo "\nPreis:" . $price;
+            /** @var $product Product */
+            $product = $this->createObject(Product::class);
+            $product
+                ->setId($idx)
+                ->setTypeId(Type::TYPE_SIMPLE)
+                ->setWebsiteIds([1])
+                ->setName('Simple Product ' . $idx)
+                ->setSku('simple' . $idx)
+                ->setPrice(mt_rand(1, 30000) / 100)
+                ->setDescription('Description')
+                ->setVisibility(Visibility::VISIBILITY_BOTH)
+                ->setStatus(Status::STATUS_ENABLED)
+                ->setStockData(['use_config_manage_stock' => 1, 'qty' => 100, 'is_qty_decimal' => 0, 'is_in_stock' => 1])
+                ->setUrlKey('url-key' . $idx)
+                ->save();
+
+            $this->productFixtures[] = $product;
+        }
+    }
+
+    /**
+     * @param string $class
+     * @return mixed
+     */
+    private function getObject($class)
+    {
+        return $this->_objectManager->get($class);
+    }
+
+    /**
+     * @param string $class
+     * @return mixed
+     */
+    private function createObject($class)
+    {
+        return $this->_objectManager->create($class);
     }
 }
