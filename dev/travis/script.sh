36aa147f00be629f3d7ffb053ffe960c6f1af2a4
Simon Gabriel <simon_gabriel@gmx.de>
2018-06-17 13:09:04 +0200

    [change] (Hackerton) Add CI config to travis script.
    Signed-off-by: Simon Gabriel <simon_gabriel@gmx.de>

--- dev/travis/script.sh
+++ dev/travis/script.sh
@@ -7,5 +7,11 @@ cd ../magento-ce
 echo '==> Doing unit tests.';
 php bin/magento dev:tests:run unit
 
-echo '==> Doing integration tests.'
+echo '==> Perform Magento integration tests.'
+php bin/magento dev:tests:run integration
+
+echo '==> Prepare Custom integration tests.'
+cp vendor/heidelpay/magento2/phpunit.xml.dist .dev/tests/integration/phpunit.xml.dist
+
+echo '==> Perform Custom integration tests.'
 php bin/magento dev:tests:run integration
