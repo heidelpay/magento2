c10d32166062ff73e88afb95444e407a7d42d358
David.Owusu <david.owusu@heidelpay.de>
2019-05-24 15:23:41 +0200

    (MAGE2-174) PIS integration: remove constructor of PISPaymentmethod

--- PaymentMethods/HeidelpayPISPaymentMethod.php
+++ PaymentMethods/HeidelpayPISPaymentMethod.php
@@ -2,30 +2,9 @@
 
 namespace Heidelpay\Gateway\PaymentMethods;
 
-use Heidelpay\Gateway\Gateway\Config\HgwBasePaymentConfigInterface;
-use Heidelpay\Gateway\Gateway\Config\HgwMainConfigInterface;
-use Heidelpay\Gateway\Gateway\Config\HgwPISPaymentConfigInterface;
-use Heidelpay\Gateway\Helper\BasketHelper;
-use Heidelpay\Gateway\Helper\Payment;
-use Heidelpay\Gateway\Model\ResourceModel\PaymentInformation\CollectionFactory as PaymentInformationCollectionFactory;
-use Heidelpay\Gateway\Model\ResourceModel\Transaction\CollectionFactory as HeidelpayTransactionCollectionFactory;
-use Heidelpay\Gateway\Model\TransactionFactory;
 use Heidelpay\PhpPaymentApi\PaymentMethods\PISPaymentMethod;
-use Magento\Framework\Api\AttributeValueFactory;
-use Magento\Framework\Api\ExtensionAttributesFactory;
-use Magento\Framework\App\ProductMetadataInterface;
-use Magento\Framework\App\RequestInterface;
-use Magento\Framework\Data\Collection\AbstractDb;
-use Magento\Framework\Encryption\Encryptor;
-use Magento\Framework\Locale\ResolverInterface;
-use Magento\Framework\Model\Context;
-use Magento\Framework\Model\ResourceModel\AbstractResource;
-use Magento\Framework\Module\ResourceInterface;
-use Magento\Framework\Registry;
-use Magento\Framework\UrlInterface;
-use Magento\Payment\Helper\Data;
-use Magento\Payment\Model\Method\Logger;
-use Magento\Sales\Helper\Data as SalesData;
+use Heidelpay\Gateway\Gateway\Config\HgwPISPaymentConfigInterface;
+use Heidelpay\Gateway\Model\TransactionFactory;
 
 /**
  * heidelpay PIS payment method
@@ -67,98 +46,14 @@ class HeidelpayPISPaymentMethod extends HeidelpayAbstractPaymentMethod
      */
     protected $_canAuthorize = true;
 
-    /**
-     * @var boolean
-     */
+    /** @var boolean */
     protected $_canRefund = true;
 
-    /**
-     * @var boolean
-     */
+    /** @var boolean */
     protected $_canRefundInvoicePartial = true;
 
-    /**
-     * HeidelpayPISPaymentMethod constructor.
-     *
-     * @param Context $context
-     * @param Registry $registry
-     * @param ExtensionAttributesFactory $extensionFactory
-     * @param AttributeValueFactory $customAttributeFactory
-     * @param Data $paymentData
-     * @param HgwMainConfigInterface $mainConfig
-     * @param RequestInterface $request
-     * @param UrlInterface $urlinterface
-     * @param Encryptor $encryptor
-     * @param Logger $logger
-     * @param ResolverInterface $localeResolver
-     * @param ProductMetadataInterface $productMetadata
-     * @param ResourceInterface $moduleResource
-     * @param HgwPISPaymentConfigInterface $paymentConfig
-     * @param Payment $paymentHelper
-     * @param BasketHelper $basketHelper
-     * @param SalesData $salesHelper
-     * @param PaymentInformationCollectionFactory $paymentInformationCollectionFactory
-     * @param TransactionFactory $transactionFactory
-     * @param HeidelpayTransactionCollectionFactory $transactionCollectionFactory
-     * @param PISPaymentMethod $PISPaymentMethod
-     * @param AbstractResource|null $resource
-     * @param AbstractDb|null $resourceCollection
-     * @param array $data
-     */
-    public function __construct(
-        Context $context,
-        Registry $registry,
-        ExtensionAttributesFactory $extensionFactory,
-        AttributeValueFactory $customAttributeFactory,
-        Data $paymentData,
-        HgwMainConfigInterface $mainConfig,
-        RequestInterface $request,
-        UrlInterface $urlinterface,
-        Encryptor $encryptor,
-        Logger $logger,
-        ResolverInterface $localeResolver,
-        ProductMetadataInterface $productMetadata,
-        ResourceInterface $moduleResource,
-        HgwBasePaymentConfigInterface $paymentConfig,
-        Payment $paymentHelper,
-        BasketHelper $basketHelper,
-        SalesData $salesHelper,
-        PaymentInformationCollectionFactory $paymentInformationCollectionFactory,
-        TransactionFactory $transactionFactory,
-        HeidelpayTransactionCollectionFactory $transactionCollectionFactory,
-        PISPaymentMethod $PISPaymentMethod,
-        AbstractResource $resource = null,
-        AbstractDb $resourceCollection = null,
-        array $data = []
-    ) {
-        parent::__construct(
-            $context,
-            $registry,
-            $extensionFactory,
-            $customAttributeFactory,
-            $paymentData,
-            $mainConfig,
-            $request,
-            $urlinterface,
-            $encryptor,
-            $logger,
-            $localeResolver,
-            $productMetadata,
-            $moduleResource,
-            $paymentHelper,
-            $basketHelper,
-            $salesHelper,
-            $paymentInformationCollectionFactory,
-            $transactionFactory,
-            $transactionCollectionFactory,
-            $resource,
-            $resourceCollection,
-            $paymentConfig,
-            $data
-        );
-
-        $this->_heidelpayPaymentMethod = $PISPaymentMethod;
-    }
+    /** @var PISPaymentMethod */
+    protected $_heidelpayPaymentMethod;
 
     /**
      * Initial Request to heidelpay payment server to get the form / iframe url
