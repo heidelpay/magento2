a855cc7f1ede4f765a9bfaff8ae1c36a0c52c088
sixer1182 <simon.gabriel@gmx.de>
2020-01-30 17:02:13 +0100

    [change] (MAGE2262) Add quote id to sales_order_grid only for heidelpay methods.

--- Model/ResourceModel/Order/Grid/Collection.php
+++ Model/ResourceModel/Order/Grid/Collection.php
@@ -2,17 +2,19 @@
 namespace Heidelpay\Gateway\Model\ResourceModel\Order\Grid;
 
 use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+use Zend_Db_Expr;
 
 class Collection extends SearchResult
 {
     protected function _initSelect()
     {
         parent::_initSelect();
-        $this->join(
-            [$this->getTable('sales_order')],
-            'main_table.entity_id = '.$this->getTable('sales_order').'.entity_id',
-            array('quote_id')
+        $subquery = new Zend_Db_Expr(
+            '(SELECT sales_order.quote_id,sales_order.entity_id FROM `sales_order_grid` AS `mt` ' .
+            'INNER JOIN `sales_order` ON mt.entity_id = sales_order.entity_id where mt.payment_method like "hgw%")'
         );
+
+        $this->getSelect()->joinLeft(['t' => $subquery], 'main_table.entity_id = t.entity_id', array('quote_id'));
 
         return $this;
     }
