413fb71105011459394e91cdc4c01a6d3c0529a7
David.Owusu <david.owusu@heidelpay.de>
2020-05-08 11:54:29 +0200

    (MAGE2-299)[refactor] Use trait for state handling of shipments.

--- PaymentMethods/Traits/CanShipBeforePayment.php
+++ PaymentMethods/Traits/CanShipBeforePayment.php
@@ -1,5 +1,16 @@
 <?php
-
+/**
+ * Trait to handle payment methods that can ship before payment e.g. Invoice.
+ *
+ * @license Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ * @link http://dev.heidelpay.com/magento2
+ * @author David Owusu
+ *
+ * @package heidelpay
+ * @subpackage magento2
+ * @category magento2
+ */
 
 namespace Heidelpay\Gateway\Traits;
 
